4c6a43da4b89777a01439173cee5e3a68df22269
Karthikeyan <75429419+Karthikeyandevp@users.noreply.github.com>
2024-03-13 21:47:28 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,17 @@
+window.botpressWebChat.init({
+    "composerPlaceholder": "Chat with EPULZ BOT",
+    "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
+    "botId": "8f66ccba-d0ca-4fd9-8972-fac7152b9cbe",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "8f66ccba-d0ca-4fd9-8972-fac7152b9cbe",
+    "webhookId": "3c03dbb0-0f9a-4c4c-bf89-7b114be93587",
+    "lazySocket": true,
+    "themeName": "prism",
+    "botName": "EPULZ BOT",
+    "stylesheet": "https://webchat-styler-css.botpress.app/prod/code/a8090586-7e3b-41d0-95b9-da8bac4de544/v89282/style.css",
+    "frontendVersion": "v1",
+    "showPoweredBy": true,
+    "theme": "prism",
+    "themeColor": "#2563eb"
+});
